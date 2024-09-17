23970f0cb844fa862e240af5d09752ac194f4f9c
Abu Sayeed <sayeed1737@gmail.com>
2024-09-17 07:37:52 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+
+# Image Carousel
+
+This project demonstrates the implementation of a responsive image carousel using the svelte-carousel package with custom buttons. The project is built with Svelte for a fast, efficient, and streamlined development process.
+
+
+## Features
+
+- Responsive Image Carousel: Built using the svelte-carousel package.
+- Custom Navigation Buttons: Easily customizable buttons for navigating between slides.
+- Built with Svelte: Utilizes Svelte to build a highly performant and modern web component.
+
+## Prerequisites
+Before you begin, ensure you have the following installed on your machine:
+
+- Node.js (v16 or higher)
+- npm (comes with Node.js)
+
+## Installation
+Follow these steps to set up and run the project locally:
+
+- Clone the repository
+`git clone <repository-url>`
+
+- Navigate to the project directory:
+`cd <project-directory>`
+
+- Install project dependencies:
+`npm install`
+
+- Install svelte-carousel package:
+`npm install svelte-carousel`
+
+- Start the development server:
+`npm run dev`
+
+- Open your browser and go to http://localhost:8080 to view the carousel.
+
---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+
+	const images = [
+		{
+			src: 'https://plus.unsplash.com/premium_photo-1681584472513-9f1f33f3928a?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            desc: 'Inspiration board'
+        },
+		{
+			src: 'https://plus.unsplash.com/premium_photo-1681680199227-b456408ae2f9?q=80&w=1413&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            desc: 'Art Mockup'
+        },
+		{
+			src: 'https://plus.unsplash.com/premium_photo-1681680198696-7bb0b3fdda0d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            desc: 'Paper-background'
+        },
+		{
+			src: 'https://plus.unsplash.com/premium_photo-1722945688629-0d31e4850b24?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            desc: 'Fishes walmate frame'
+        },
+		{
+			src: 'https://plus.unsplash.com/premium_photo-1664391914821-29ee0b3d98ee?q=80&w=1510&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            desc: 'Interior design'
+        },
+		
+	];
+
+</script>
+
+<main>
+	<h1>Image Carousel</h1>
+
+</main>
+
+<style>
+
+</style>
